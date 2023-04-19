ad6f243ccdb341cd3c52e87e5e1d4dce698519a9
Sepro <72236405+seprogramd14@users.noreply.github.com>
2023-04-19 09:13:18 +0900

    [level 0] Title: 공 던지기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.py
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.py
@@ -1,6 +1,2 @@
 def solution(numbers, k):
-    answer = 0
-    for _ in range(k-1):
-        answer += 2
-        if answer > len(numbers)-1: answer -= len(numbers)
-    return numbers[answer]
+    return numbers[2 * (k-1) % len(numbers)]
