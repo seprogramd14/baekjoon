0739f25049822979584d09a815b1d6951c8c9990
Sepro <72236405+seprogramd14@users.noreply.github.com>
2023-04-24 17:37:01 +0900

    [level 0] Title: 가까운 수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.py
@@ -1,11 +1,4 @@
 def solution(array, n):
-    answer = 0
-    array.append(n)
-    array.sort()
-    idx = array.index(n)
-    if 0 < idx < len(array)-1:
-        if array[idx + 1] - array[idx] < array[idx] - array[idx-1]: answer = array[idx + 1]
-        else: answer = array[idx-1]
-    elif idx == 0: answer = array[1]
-    else: answer = array[-2]
-    return answer
+    # 각 값의 격차를 확인
+    array.sort(key = lambda x : (abs(x-n), x-n)) # 마이너스는 절댓값으로
+    return array[0]
