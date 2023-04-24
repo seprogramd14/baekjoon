fa96180f10dcc0ed081056de49c24952798a25a9
Sepro <72236405+seprogramd14@users.noreply.github.com>
2023-04-24 17:10:44 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.py
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.py
@@ -1,8 +1,6 @@
 def solution(my_string):
     answer = ''
-    dic = {i:0 for i in "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz "}
     for i in my_string:
-        if dic[i] == 0:
-            dic[i] += 1
+        if i not in answer:
             answer += i
     return answer
