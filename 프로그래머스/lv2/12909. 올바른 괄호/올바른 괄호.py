267a78c7c857459d9e99bc8ded7d0b5fc7994f43
Sepro <72236405+seprogramd14@users.noreply.github.com>
2023-05-08 16:54:48 +0900

    Update 올바른 괄호.py

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,7 +1,6 @@
 def solution(s):
-    answer = True
     stack = []
     for w in s:
         if w == ')': stack.pop() if stack else stack.append(w)
         elif w == '(': stack.append(w)
-    return False if stack else True
+    return bool(not stack)
