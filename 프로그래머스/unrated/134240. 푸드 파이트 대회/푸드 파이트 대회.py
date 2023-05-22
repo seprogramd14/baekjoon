eede65f308e0a9860bc69287e1ca3c04a87796e3
Sepro <72236405+seprogramd14@users.noreply.github.com>
2023-05-22 20:02:27 +0900

    Update 푸드 파이트 대회.py

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -2,4 +2,4 @@ def solution(food):
     answer = ''
     for i in range(1, len(food)):
         answer += str(i) * (food[i] // 2)
-    return answer += '0' + answer[::-1] # 이거 기억 좀 하자.
+    return answer + '0' + answer[::-1] # 이거 기억 좀 하자.
