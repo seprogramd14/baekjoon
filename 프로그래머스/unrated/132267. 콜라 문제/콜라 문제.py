e26c465bd2d756527b8728c6c28f37387b6eff99
Sepro <72236405+seprogramd14@users.noreply.github.com>
2023-05-22 19:30:39 +0900

    [unrated] Title: 콜라 문제, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
@@ -1,6 +1,4 @@
 def solution(a, b, n):
     answer = 0
-    while True:
-        answer, n = n//a * b + answer, n - (n//a) * (a-b)
-        if n // a <= 0: break
+    while n // a > 0: answer, n = n//a * b + answer, n - (n//a) * (a-b)
     return answer
