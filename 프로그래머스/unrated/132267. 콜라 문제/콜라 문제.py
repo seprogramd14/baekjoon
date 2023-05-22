88c43627c29f80d09878c16f5e04b1e7417f43a9
Sepro <72236405+seprogramd14@users.noreply.github.com>
2023-05-22 19:29:50 +0900

    Update 콜라 문제.py

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
@@ -1,6 +1,6 @@
 def solution(a, b, n):
     answer = 0
     while True:
-        answer, n = n//a * b + answer, n - (n//a) * a + (n//a) * b
+        answer, n = n//a * b + answer, n - (n//a) * (a-b)
         if n // a <= 0: break
     return answer
