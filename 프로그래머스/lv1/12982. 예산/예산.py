6f84b7f94944385dfefce223ec16c8b192db13a7
Sepro <72236405+seprogramd14@users.noreply.github.com>
2023-05-31 17:29:26 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 9.95 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.py
+++ 프로그래머스/lv1/12982. 예산/예산.py
@@ -2,6 +2,5 @@ def solution(d, budget):
     answer = 0
     for i in sorted(d):
         if budget >= i:
-            budget -= i
-            answer += 1
+            budget, answer = budget - i, answer + 1
     return answer
