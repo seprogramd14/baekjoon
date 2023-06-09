452b3f200366cff5e0a0ea7e137eabe33d1e16b4
Sepro <72236405+seprogramd14@users.noreply.github.com>
2023-06-09 14:08:07 +0900

    [level 0] Title: k의 개수, Time: 10.06 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.py
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.py
@@ -1,6 +1,4 @@
 def solution(i, j, k):
     answer = 0
-    for num in range(i, j+1):
-        num = list(str(num))
-        answer += num.count(str(k))
+    for num in range(i, j+1): answer += str(num).count(str(k))
     return answer
