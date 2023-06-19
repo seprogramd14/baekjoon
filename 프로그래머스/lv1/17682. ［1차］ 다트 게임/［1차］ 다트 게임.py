9bb733a9ee3c8baf97b1996a80db2b66ba8c8ee1
Sepro <72236405+seprogramd14@users.noreply.github.com>
2023-06-19 18:12:40 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -0,0 +1,18 @@
+def solution(dartResult):
+    answer, save, cut = 0, [], 0
+    for i in range(len(dartResult)):
+        try:
+            if dartResult[i] == '1' and dartResult[i+1] == '0':
+                save.append(10)
+                cut = 1
+            elif cut == 1: cut = 0
+            else: save.append(int(dartResult[i]))
+        except:
+            if dartResult[i] == "S": save[-1] **= 1
+            elif dartResult[i] == "D": save[-1] **= 2
+            elif dartResult[i] == "T": save[-1] **= 3
+            elif dartResult[i] == "*":
+                save[-1] *= 2
+                if len(save) > 1: save[-2] *= 2
+            elif dartResult[i] == "#": save[-1] *= -1
+    return sum(save)
