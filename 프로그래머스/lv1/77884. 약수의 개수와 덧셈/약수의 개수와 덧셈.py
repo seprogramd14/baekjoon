57ce3e4cf5457abad598cdd2bd07bce85bca8277
Sepro <72236405+seprogramd14@users.noreply.github.com>
2023-09-08 10:25:41 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,12 +1,8 @@
 def solution(left, right):
     answer = 0
     for i in range(left, right+1):
-        count = 0
-        for j in range(1, i+1):
-            if i % j == 0:
-                count += 1
-        if count % 2 == 0:
-            answer += i
-        else:
+        if int(i**0.5) == i**0.5:
             answer -= i
+        else:
+            answer += i
     return answer
