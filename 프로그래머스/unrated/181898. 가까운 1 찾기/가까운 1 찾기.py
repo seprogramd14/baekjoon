15762d0552ef48e2bbab74425f8c65917585b432
Sepro <72236405+seprogramd14@users.noreply.github.com>
2023-10-04 17:15:38 +0900

    [unrated] Title: 가까운 1 찾기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.py
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.py
@@ -1,3 +1,4 @@
 def solution(arr, idx):
-    answer = [i for i in range(idx, len(arr)) if arr[i] == 1]
-    return answer[0] if answer else -1
+    for i in range(idx, len(arr)):
+        if arr[i] == 1: return i
+    return -1
