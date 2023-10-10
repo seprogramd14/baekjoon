8e9268140c69715237b4a473f5dce54fe242e919
Sepro <72236405+seprogramd14@users.noreply.github.com>
2023-10-10 17:20:13 +0900

    [level unrated] Title: 배열 만들기 6, Time: 21.45 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.py
@@ -0,0 +1,12 @@
+def solution(arr):
+    stk, i = [], 0
+    while i < len(arr):
+        if not stk:
+            stk.append(arr[i])
+        else:
+            if stk[-1] == arr[i]:
+                stk.pop()
+            else:
+                stk.append(arr[i])
+        i += 1
+    return stk if stk else [-1]
