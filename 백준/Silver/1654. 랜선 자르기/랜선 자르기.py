201efd480afbfd3d5eb31f84fd596853799a7e2f
Sepro <72236405+seprogramd14@users.noreply.github.com>
2023-11-03 15:03:42 +0900

    [Silver II] Title: 랜선 자르기, Time: 104 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+n, k = list(map(int, input().split()))
+cables = sorted([int(input()) for _ in range(n)])
+
+start = 1
+end = sum(cables)
+
+while start <= end:
+	mid = (start + end) // 2
+
+	count = 0
+	for cable in cables:
+		count += cable // mid
+	
+	if count >= k:
+		start = mid + 1
+	else:
+		end = mid - 1
+	
+print(end)
