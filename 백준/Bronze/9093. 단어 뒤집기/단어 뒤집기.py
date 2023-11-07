55d50a885ba1cb6230cbd2b0b036fd7a7867ac0b
Sepro <72236405+seprogramd14@users.noreply.github.com>
2023-11-07 17:09:30 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 920 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
@@ -1,7 +1,5 @@
 n = int(input())
-
 for i in range(n):
-  string = list(map(list, input().split()))
-  for j in range(len(string)):
-    string[j].reverse()
-    print("".join(s for s in string[j]), end=" ")
+    string = list(map(list, input().split()))
+    for j in range(len(string)):
+        print("".join(s for s in string[j][::-1]), end=" ")
