4c893ccfb8842aa62e3fd57d467becd2898fc3eb
Sepro <72236405+seprogramd14@users.noreply.github.com>
2024-03-15 10:34:47 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -1,6 +1,3 @@
 from itertools import combinations
 def solution(number):
-    answer = 0
-    for c in combinations(number, 3):
-        answer += 0 if sum(c) else 1
-    return answer
+    return sum([0 if sum(c) else 1 for c in combinations(number, 3)])
