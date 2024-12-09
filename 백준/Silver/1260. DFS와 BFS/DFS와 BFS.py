413094be747681a813125b27684032cf8068a469
Sepro <72236405+seprogramd14@users.noreply.github.com>
2024-12-09 04:04:39 -0800

    [Silver II] Title: DFS와 BFS, Time: 68 ms, Memory: 35580 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -2,49 +2,42 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 
-def dfs(graph, root, visited = []):
-    visited.append(root)
-    for node in graph[root]:
-        if node not in visited:
-            dfs(graph, node, visited)
-    return visited
+n, m, v = map(int, input().split())
+graph = {i+1:[] for i in range(n)}
 
-def bfs(graph, root):
-    visited = []
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[b].append(a)
+    graph[a].append(b)
+
+for i in range(n):
+    graph[i+1].sort()
+
+def dfs(root):
+    res = []
+    stack = [root]
+    visited = [0]*(n+1)
+    while stack:
+        node = stack.pop()
+        if not visited[node]:
+            visited[node] = 1
+            stack.extend(list(reversed(graph[node])))
+            res.append(node)
+    return res
+
+def bfs(root):
+    res = [root]
     queue = deque([root])
-
+    visited = [0]*(n+1)
+    visited[root] = 1
     while queue:
         node = queue.popleft()
-        if node not in visited:
-            visited.append(node)
-            queue.extend(graph[node])
+        for g in graph[node]:
+            if not visited[g]:
+                visited[g] = 1
+                queue.append(g)
+                res.append(g)
+    return res
 
-    return visited
-
-N, M, V = list(map(int, input().split()))
-graph = dict()
-
-for i in range(M):
-    edge = list(map(int, input().split()))
-    key = graph.keys()
-    if edge[0] not in key:
-        graph[edge[0]] = [edge[1]]
-    else:
-        graph[edge[0]] += [edge[1]]
-
-    if edge[1] not in key:
-        graph[edge[1]] = [edge[0]]
-    else:
-        graph[edge[1]] += [edge[0]]
-
-graph_key = graph.keys()
-
-for i in graph_key:
-    graph[i].sort()
-
-if V in graph_key:
-    print(*dfs(graph, V))
-    print(*bfs(graph, V))
-else:
-    print(V)
-    print(V)
+print(*dfs(v))
+print(*bfs(v))
