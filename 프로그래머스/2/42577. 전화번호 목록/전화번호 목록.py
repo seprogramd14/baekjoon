0e0c318e4d274e56a425bca0dd77b5246be8ced4
Sepro <72236405+seprogramd14@users.noreply.github.com>
2025-01-18 22:49:06 +0900

    [level 2] Title: 전화번호 목록, Time: 53.50 ms, Memory: 46.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -2,6 +2,6 @@ def solution(phone_book):
     dic = {b:0 for b in phone_book}
     for l in set([len(b) for b in phone_book]):
         for b in phone_book:
-            if len(b) > l and b[:l] in dic.keys():
+            if len(b) > l and b[:l] in dic:
                 return False
     return True
